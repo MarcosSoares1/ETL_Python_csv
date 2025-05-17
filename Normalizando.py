51758c261a3d3dfd4ef69de6a8ca5562d38ab085
MarcosSoares1 <marcossoares.oliveira@outlook.com>
2025-05-17 14:46:35 -0300

    update

--- Normalizando.py
+++ Normalizando.py
@@ -11,9 +11,11 @@ df["Desconto"] = df["Desconto"].fillna(0)
 # Calcular o total de vendas
 df["Total"] = df["Preco"] * df["Quantidade"] * (1 - df["Desconto"] / 100)
 
+df["Total"] = df["Total"].apply(lambda x: f"R$ {x:,.2f}")
 # Salvar os dados tratados em um novo arquivo CSV
 df.to_csv("dados_tratados.csv", index=False)
 
 # Exibir confirmação e amostra dos dados
 print("Dados tratados e salvos em 'dados_tratados.csv'.")
-print(df.head())  # Exibe as primeiras linhas para conferir
+# Exibe as primeiras linhas para conferir
+print(df.head()) 

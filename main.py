e2bc677db033b2f9aabddc47a23b0bad7dca3ea5
Marcos Soares <143642004+MarcosSoares1@users.noreply.github.com>
2025-05-20 09:48:07 -0300

    Atualizando

--- main.py
+++ main.py
@@ -1,9 +1,2 @@
 import pandas as pd
 df = pd.read_csv("dados.csv")
-
-#Limpeza e transformação dos dados
-# - Remover valores nulos
-# - Renomear colunas
-# - Criar novas colunas derivadas
-
-df.to_csv("dados_transformados.csv", index=False) 
